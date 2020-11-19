33b5ea9cbd00e74db4eb4b5f4bf98e04c84967c5
Brad Thorson <bbthorson@gmail.com>
2020-11-18 18:31:44 -0600

    removed container from <LandingPage />

--- frontend/src/marketing/layout/Footer.tsx
+++ frontend/src/marketing/layout/Footer.tsx
@@ -26,10 +26,9 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   footerArea: {
-    borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: theme.spacing(8),
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
+    marginTop: theme.spacing(4),
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(2),
     [theme.breakpoints.up('sm')]: {
       paddingTop: theme.spacing(6),
       paddingBottom: theme.spacing(6),
