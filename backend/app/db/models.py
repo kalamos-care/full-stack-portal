3062bda73b93d954aba51423bd6a15ce43540d72
Brad Thorson <bbthorson@gmail.com>
2020-12-18 16:35:18 -0600

    updated back_populates in models

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -25,6 +25,7 @@ class Address(Base):
     __tablename__ = "address"
 
     id = Column(Integer, primary_key=True, index=True)
+    address_id = Column(Integer)
     title = Column(String)
     street_1 = Column(String)
     street_2 = Column(String)
@@ -40,21 +41,22 @@ class Provider(Base):
     __tablename__ = "provider"
 
     id = Column(Integer, primary_key=True, index=True)
+    provider_id = Column(Integer)
     first_name = Column(String)
     last_name = Column(String)
     email = Column(String, unique=True)
     phone = Column(String)
     # this seems very incorrect...
-    address = relationship("Address")
+    address = relationship("Address", back_populates="address_id")
     npi = Column(Integer, unique=True)
     medicaid_id = Column(String)
     # does this need to be a join table?
-    state_licenses = relationship("State_License")
+    state_licenses = relationship("State_License", back_populates="provider_id")
     mtl_provider_id = Column(String, unique=True)
     is_public = Column(Boolean, default=False)
     is_accepting_patients = Column(Boolean, default=True)
-    patients = relationship("Provider__Patient")
-    clinics = relationship("Provider__Clinic")
+    patients = relationship("Provider__Patient", back_populates="patient_id")
+    clinics = relationship("Provider__Clinic", back_populates="clinic_id")
 
 
 # State Licenses
@@ -71,8 +73,9 @@ class Clinic(Base):
     __tablename__ = "clinic"
 
     id = Column(Integer, primary_key=True, index=True)
+    clinic_id = Column(Integer)
     legal_name = Column(String)
-    legal_address = relationship("Address")
+    legal_address = relationship("Address", back_populates="address_id")
     contact_name = Column(String)
     contact_email = Column(String)
     contact_phone = Column(String)
@@ -83,8 +86,8 @@ class Clinic(Base):
     is_public = Column(Boolean)
     is_accepting_patients = Column(Boolean)
     has_BAA = Column(Boolean)
-    providers = relationship("Provider__Clinic")
-    patients= relationship("Clinic__Patient")
+    providers = relationship("Provider__Clinic", back_populates="provider_id")
+    patients= relationship("Clinic__Patient", back_populates="patient_id")
 
 
 # Clinics Users
@@ -92,16 +95,17 @@ class Clinic__User(Base):
     __tablename__ = "clinic__user"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = relationship("User")
-    clinic_id = relationship("Clinic")
+    user_id = relationship("User", , back_populates="id")
+    clinic_id = relationship("Clinic", back_populates="clinic_id")
 
 # Labs
 class Lab(Base):
     __tablename__ = "lab"
 
     id = Column(Integer, primary_key=True, index=True)
+    lab_id = Column(Integer)
     legal_name = Column(String)
-    legal_address = relationship("Address")
+    legal_address = relationship("Address", back_populates="address_id")
     contact_name = Column(String)
     contact_email = Column(String)
     contact_phone = Column(String)
@@ -109,7 +113,7 @@ class Lab(Base):
     billing_contact_email = Column(String)
     billing_contact_phone = Column(String)
     url = Column(String)
-    assays = relationship("Lab__Assay")
+    assays = relationship("Lab__Assay", , back_populates="assay_id")
 
 
 # Lab__Assay
@@ -118,8 +122,8 @@ class Lab__Assay(Base):
     __tablename__ = "lab__assay"
 
     id = Column(Integer, primary_key=True, index=True)
-    lab_id = relationship("Lab")
-    assay_id = relationship("Assay")
+    lab_id = relationship("Lab", back_populates="lab_id")
+    assay_id = relationship("Assay", back_populates="assay_id")
 
 
 # Patients
@@ -127,6 +131,7 @@ class Patient(Base):
     __tablename__ = "patient"
 
     id = Column(Integer, primary_key=True, index=True)
+    patient_id = Column(Integer)
     legal_first_name = Column(String)
     preferred_first_name = Column(String)
     legal_last_name = Column(String)
@@ -141,14 +146,14 @@ class Patient(Base):
     email = Column(String)
     phone_day = Column(String)
     phone_night = Column(String)
-    social = relationship("Patient__Social")
-    providers = relationship("Provider__Patient")
-    labs_orders = relationship("Lab_Order")
-    prescriptions = relationship("Prescription__Patient")
+    social = relationship("Patient__Social", back_populates="network_id")
+    providers = relationship("Provider__Patient", back_populates="provider_id")
+    labs_orders = relationship("Lab_Order", back_populates="lab_order_number")
+    prescriptions = relationship("Prescription__Patient", back_populates="prescription_id")
     labs_hipaa_auth = Column(Boolean)
     labs_hipaa_auth_date = Column(Date)
-    shipping_address = relationship("Addess")
-    home_address = relationship("Addess")
+    shipping_address = relationship("Addess", back_populates="address_id")
+    home_address = relationship("Addess", back_populates="address_id")
 
 
 # Patient__Social
@@ -156,16 +161,17 @@ class Patient__Social(Base):
     __tablename__ = "patient__social"
 
     id = Column(Integer, primary_key=True, index=True)
-    patient = relationship("Patient")
-    facebook = Column(String)
+    patient = relationship("Patient", back_populates="patient_id")
+    network_id = Column(String)
 
 # Prescriptions
 class Prescription__Patient(Base):
     __tablename__ = "prescription__patient"
 
     id = Column(Integer, primary_key=True, index=True)
-    patient_id = relationship("Patient")
-    provider_id = relationship("Provider")
+    prescription_id = Column(Integer)
+    patient_id = relationship("Patient", back_populates="patient_id")
+    provider_id = relationship("Provider", back_populates="provider_id")
     rx_name = Column(String)
     rx_number = Column(String)
     rx_quantity = Column(Integer)
@@ -180,8 +186,8 @@ class Provider__Patient(Base):
     __tablename__ = "provider__patient"
 
     id = Column(Integer, primary_key=True, index=True)
-    patient_id = relationship("Patient")
-    provider_id = relationship("Provider")
+    patient_id = relationship("Patient", back_populates="patient_id")
+    provider_id = relationship("Provider", back_populates="provider_id")
     hipaa_auth = Column(Boolean)
     hipaa_auth_expire = Column(Date)
 
@@ -191,8 +197,8 @@ class Clinic__Patient(Base):
     __tablename__ = "clinic__patient"
 
     id = Column(Integer, primary_key=True, index=True)
-    clinic_id = relationship("Clinic")
-    patient_id = relationship("Patient")
+    clinic_id = relationship("Clinic", back_populates="clinic_id")
+    patient_id = relationship("Patient", back_populates="patient_id")
     hipaa_auth = Column(Boolean)
     hipaa_auth_expire = Column(Date)
 
@@ -202,8 +208,8 @@ class Provider__Clinic(Base):
     __tablename__ = "provider__clinic"
 
     id = Column(Integer, primary_key=True, index=True)
-    provider_id = relationship("Provider")
-    clinic_id = relationship("Clinic")
+    provider_id = relationship("Provider", back_populates="provider_id")
+    clinic_id = relationship("Clinic", back_populates="clinic_id")
 
 
 # Lab__Patient
@@ -215,8 +221,8 @@ class Lab__Patient(Base):
     __tablename__ = "lab__patient"
 
     id = Column(Integer, primary_key=True, index=True)
-    patient_id = relationship("Patient")
-    lab_id = relationship("Lab")
+    patient_id = relationship("Patient", back_populates="patient_id")
+    lab_id = relationship("Lab", back_populates="lab_id")
     hipaa_auth = Column(Boolean)
     hipaa_auth_expire = Column(Date)
 
@@ -226,12 +232,13 @@ class Assay(Base):
     __tablename__ = "assay"
 
     id = Column(Integer, primary_key=True, index=True)
+    assay_id = Column(Integer)
     long_name = Column(String)
     short_name = Column(String)
     loinc_code = Column(String)
     cpt_code = Column(String)
-    lab_id = relationship("Lab")
-    device_id = relationship("Device")
+    lab_id = relationship("Lab", back_populates="lab_id")
+    device_id = relationship("Device", back_populates="device_id")
     cash_price = Column(Float)
     is_available = Column(Boolean)
 
@@ -241,14 +248,14 @@ class Lab_Order(Base):
     __tablename__ = "lab_order"
 
     id = Column(Integer, primary_key=True, index=True)
-    patient_id = relationship("Patient")
+    patient_id = relationship("Patient", back_populates="patient_id")
     # does this need a join table? We might want to look up all the lab orders per patient?
     lab_order_number = Column(Integer)
-    provider_id = relationship("Provider")
+    provider_id = relationship("Provider", back_populates="provider_id")
     # same question as above
-    clinic_id = relationship("Clinic")
+    clinic_id = relationship("Clinic", back_populates="clinic_id")
     # same question as above
-    assays = relationship("Order__Assays")
+    assays = relationship("Order__Assays", back_populates="assay_id")
     #
     # need to add this at some point
     # icd_10_codes = Column(List)
@@ -266,8 +273,8 @@ class Order__Assays(Base):
     __tablename__ = "order__assays"
 
     id = Column(Integer, primary_key=True, index=True)
-    lab_order_id = relationship("Lab_Order")
-    assay_id = relationship("Assay")
+    lab_order_id = relationship("Lab_Order", back_populates="lab_order_number")
+    assay_id = relationship("Assay", back_populates="assay_id")
 
 
 # Order__ICD10
@@ -292,14 +299,14 @@ class Kit(Base):
     kit_name = Column(String)
     kit_sku = Column(String)
     kit_id = Column(String)
-    devices = relationship("Kit__Device")
+    devices = relationship("Kit__Device", back_populates="device")
     order_id = Column(Integer)
     shipping_speed = Column(Integer)
     status = Column(String)
-    outbound_address = relationship("Address")	
+    outbound_address = relationship("Address", back_populates="address_id")	
     outbound_tracking = Column(String)
     delivered_to_recepient = Column(Boolean)
-    inbound_address = relationship("Address")
+    inbound_address = relationship("Address", back_populates="address_id")
     inbound_tracking = Column(String)
     delivered_to_processor = Column(Boolean)
 
@@ -309,17 +316,17 @@ class Kit__Device(Base):
     __tablename__ = "kit__device"
 
     id = Column(Integer, primary_key=True, index=True)
-    kit_id = relationship("Kit")
-    device = relationship("Device")
+    kit_id = relationship("Kit", back_populates="kit_id")
+    device = relationship("Device", back_populates="device_id")
 
 
 # Insurers
 class Insurer(Base):
     __tablename__ = "insurer"
 
-    id = Column(Integer, primary_key=True, index=True)
+    insurer_id = Column(Integer, primary_key=True, index=True)
     insurer_name = Column(String)
-    claims_address = relationship("Address")
+    claims_address = relationship("Address", back_populates="address_id")
     edi	= Column(Integer)
 
 
@@ -328,8 +335,8 @@ class Insurer__Patient(Base):
     __tablename__ = "insurer__patient"
 
     id = Column(Integer, primary_key=True, index=True)
-    patient_id = relationship("Patient")
-    insurer_id = relationship("Insurer")
+    patient_id = relationship("Patient", back_populates="patient_id")
+    insurer_id = relationship("Insurer", back_populates="insurer_id")
     member_id = Column(String)
     group_id = Column(String)
     pbm_bin = Column(String)
